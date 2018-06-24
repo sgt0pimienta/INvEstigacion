ebf63ac9e859a78e26f176febe748a9db0b3f078
sgt0pimienta <andres.dlape@gmail.com>
2018-06-24 00:32:58 -0500

    Añadiendo métodos para descargar los Jsons
    Añadiendo métodos para pasar los Jsons interpretados a archivos de texto con puro texto o en formato CSV

--- INvEstigacion/Program.cs
+++ INvEstigacion/Program.cs
@@ -18,23 +18,59 @@ namespace INvEstigacion
 {
     class Program
     {
+        //Jalisco = 14, CDMX = 9, Yucatán = 31, NL = 19
         private WebClient downloader;
-        private List<string> seccionesJalisco;
-        private List<string> seccionesMty;
+        private List<string> seccionesJal;
+        private List<string> seccionesNln;
         private List<string> seccionesYcn;
-        private List<string> seccionesCDMX;
+        private List<string> seccionesCdmx;
+        private List<JsonSeccion> listaJsons;
 
         static void Main(string[] args)
         {
 
         }
 
-        public void SacarJsons(List<string> listaDeSecciones)
+        public void SacarJsons(List<string> listaDeSecciones, string estado)
         {
+            int cuentaDeSecciones = 0;
+            string codigoEstado;
+            List<string> jsonEstado;
+
+            switch (estado)
+            {
+                case "Jalisco": codigoEstado = "14"; break;
+                case "Nuevo Leon": codigoEstado = "19"; break;
+                case "Yucatan": codigoEstado = "31"; break;
+                case "CDMX": codigoEstado = "9"; break;
+                default: throw new Exception("link malo. O eres mala persona o no hay ningún código de estado asignado al estado que intentaste buscar (el programa no soporta ese estado)");
+            }
+
             foreach (string seccion in listaDeSecciones)
             {
-                //downloader.DownloadFile();    
+                cuentaDeSecciones += 1;
+                var jsonDescargado = downloader.DownloadString("https://ubicatucasilla.ine.mx/api/casillas/" + codigoEstado + "/" + cuentaDeSecciones + "/casillas.json");
+                JsonSeccion datosSeccion = JsonConvert.DeserializeObject<JsonSeccion>(jsonDescargado);
+                listaJsons.Add(datosSeccion);
             }
         }
+
+        public void JsonsToText(List<JsonSeccion> lista, string tipo)
+        {
+            if (tipo == "csv")
+            {
+                string csv = String.Join(",", lista);
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    class JsonSeccion
+    {
+
     }
 }
